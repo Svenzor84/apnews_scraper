3f02a48dc05d13115e6df7ed781b792d9e73f4e1
Svenzor84 <steve.rossbyers@gmail.com>
2021-09-16 07:27:23 -0700

    Converted all promise logic to utilize await over .then

--- article_scraper.js
+++ article_scraper.js
@@ -10,43 +10,39 @@ const url = `${BASE_URL}/hub/${section}?utm_source=apnewsnav&utm_medium=sections
 
 // Launch a puppeteer browser instance
 const browser = await puppeteer.launch({ignoreDefaultArgs: ['--disable-extensions']});
+try {
+    // Direct the browser to the AP News Section page we want to scrape
+    let dynamic_page = await browser.newPage()
+    await dynamic_page.goto(url)
+    // Scroll to the bottom of the page so that all article images load
+    await utils.autoScroll(dynamic_page);
+    // Take a screenshot to review (for debugging)
+    await dynamic_page.screenshot({
+        path:`screenshots/${Date.now()}.png`,
+        fullPage: true
+    });
+    // Return the loaded page content to be scraped
+    const html = await dynamic_page.content();
+    // Load the page content into cheerio
+    const page = await cheerio.load(html);
+            
+    // Pull an array of all FeedCard elements on the loaded page
+    const articles = page(SELECTORS.FEEDCARD);
+    console.log(`Articles found: ${articles.length}`);
+            
+    let article_objects = [];
 
-// Direct the browser to the AP News Section page we want to scrape
-await browser.newPage()
-    .then(page => {
-        return page.goto(url).then( async () => {
-            // Scroll to the bottom of the page so that all article images load
-            await utils.autoScroll(page);
-            // Take a screenshot to review (for debugging)
-            await page.screenshot({
-                path:'screen.png',
-                fullPage: true
-            });
-            // Return the loaded page content to be scraped
-            return page.content();
-        });
-    })
-    .then(html => {
-        // Load the page content into cheerio
-        const page = cheerio.load(html);
-        
-        // Pull an array of all FeedCard elements on the loaded page
-        const articles = page(SELECTORS.FEEDCARD);
-        console.log(`Articles found: ${articles.length}`);
-        
-        let article_objects = [];
-
-        // Loop through the identified FeedCard elements to parse desired article data
-        articles.each(function() {
-            // Parse article details from each FeedCard
-            let article_data = utils.parseArticle(this, page);
-            // Add each article to the objects array
-            article_objects.push(article_data);
-        });
-        console.log(`Total Article Objects Generated: ${article_objects.length}`);
-        console.log(article_objects);
-    })
-.catch(console.error);
-
-// Close the browser instance
-browser.close();
+    // Loop through the identified FeedCard elements to parse desired article data
+    articles.each(function() {
+        // Parse article details from each FeedCard
+        let article_data = utils.parseArticle(this, page);
+        // Add each article to the objects array
+        article_objects.push(article_data);
+    });
+    console.log(`Total Article Objects Generated: ${article_objects.length}`);
+    console.log(article_objects);
+} catch (error) {
+    console.error(error);
+} finally {
+    await browser.close();
+}
