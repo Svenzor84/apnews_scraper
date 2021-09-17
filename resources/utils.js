c192a2d9e92939a7617be6995461d80d6660c1b3
Svenzor84 <steve.rossbyers@gmail.com>
2021-09-16 20:33:00 -0700

    Migrated report generation logic into its own method

--- resources/utils.js
+++ resources/utils.js
@@ -1,14 +1,15 @@
-import { SELECTORS, BASE_URL } from './constants.js';
+import { SELECTORS, BASE_URL, BASE_START_HTML, BASE_END_HTML } from './constants.js';
 import moment from 'moment';
+import * as fs from 'fs';
 
 export default class Utils {
-    constructor() {}
+    constructor() { }
 
     /**
      * Scrolls to the bottom of a page to dynamically load all content
      * @param {*} page A puppetteer page object with loaded html
      */
-    async autoScroll(page){
+    async autoScroll(page) {
         await page.evaluate(async () => {
             await new Promise((resolve, reject) => {
                 let totalHeight = 0;
@@ -22,7 +23,7 @@ export default class Utils {
                     window.scrollBy(0, distance);
                     totalHeight += distance;
                     // Once we have scrolled to the bottom of the page we can resolve the promise
-                    if(totalHeight >= scrollHeight){
+                    if (totalHeight >= scrollHeight) {
                         clearInterval(timer);
                         resolve();
                     }
@@ -30,7 +31,7 @@ export default class Utils {
             });
         });
     }
-    
+
     /**
      * Parses a string of one or more Authors to generate an array of author names
      * @param {string} author_string String of authors (comma separated) to be parsed
@@ -44,7 +45,7 @@ export default class Utils {
         let authors_array = [];
         authors_split.forEach((author) => {
             // The substring " and " is used to separate the last two authors in the list
-            if(author.includes(' and ')) {
+            if (author.includes(' and ')) {
                 const secondary_split = author.split(' and ');
                 secondary_split.forEach((add_author) => {
                     authors_array.push(add_author);
@@ -73,9 +74,6 @@ export default class Utils {
         // Check for case where no image is provided
         if (image_src !== undefined) {
             article_data.image = image_src;
-        } else {
-            // Use the default (AP Logo) image path if no image is provided
-            article_data.image = 'resources/ap_logo.bmp';
         }
         // Author data lives in the ByLine element's text node nested in the FeedCard
         let authors_text = page(parsed_article).find(SELECTORS.BYLINE).text()
@@ -92,7 +90,57 @@ export default class Utils {
         article_data.datetime = moment(timestamp.attr('data-source'));
         // Relative Date (x hours ago, yesterday, etc) data lives in the Timestamp element's text node
         article_data.datetime_relative = timestamp.text();
-        
+
         return article_data;
+    }
+
+    /**
+     * Generates an HTML report (list) of parsed articles (file generated in /output)
+     * @param {*} section String representation of the AP News section that was parsed
+     * @param {*} article_objects Array of article objects with parsed data
+     */
+    generateReport(section, article_objects) {
+        const fileName = `./output/${section}_articles.html`;
+        const stream = fs.createWriteStream(fileName);
+
+        // Write the basic HTML start template to the file
+        stream.write(BASE_START_HTML, 'UTF8');
+
+        stream.write(`<h1>AP News ${section.charAt(0).toUpperCase()}${section.slice(1)} Articles</h1>`);
+        // Here is where we build the actual HTML report
+        article_objects.forEach((article) => {
+            stream.write('<div>', 'UTF8');
+            if (article.image !== undefined) {
+                stream.write(`<img src=${article.image} >`);
+            }
+            stream.write('<span class="article_title">', 'UTF8');
+            stream.write(`<a href=${article.link}>${article.title}</a>`, 'UTF8');
+            stream.write('</span>', 'UTF8');
+            stream.write('</div>', 'UTF8');
+
+            stream.write('<span class="author">');
+            article.authors.forEach((author) => {
+                stream.write(`| ${author} |`);
+            })
+            stream.write('</span>');
+            stream.write(`<span class="datetime"> from ${article.datetime_relative} (${article.datetime})</span>`);
+
+            stream.write('<hr>');
+        });
+
+        // Write the basic HTML end template to the file
+        stream.write(BASE_END_HTML, 'UTF8');
+
+        // Mark the end of file
+        stream.end();
+
+        // Handle stream events --> finish, and error
+        stream.on('finish', function () {
+            console.log("Report Generated.");
+        });
+
+        stream.on('error', function (err) {
+            console.log(err.stack);
+        });
     }
 }
