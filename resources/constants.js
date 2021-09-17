a2d3443fd5f8a785fc87b56a75ff2cc6f56281de
Svenzor84 <steve.rossbyers@gmail.com>
2021-09-16 20:20:26 -0700

    Updated base html tags

--- resources/constants.js
+++ resources/constants.js
@@ -7,5 +7,5 @@ export const SELECTORS = {
 
 export const BASE_URL = 'http://www.apnews.com';
 
-export const BASE_START_HTML = '<html><head><meta><title>AP News Parser Report</title></meta></head><body>';
+export const BASE_START_HTML = '<!DOCTYPE html><html lang="en"><head><link rel="stylesheet" href="../resources/style.css"><meta charset="utf-8"/><title>AP News Parser Report</title></head><body>';
 export const BASE_END_HTML = '</body></html>';
